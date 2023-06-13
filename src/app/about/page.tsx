f3ca645bcc673918d9688f49cbb9f8c6753c32d5
Felipe Suarez <felipesuarez1525@gmail.com>
2023-06-13 00:50:39 -0300

    add services mobile view

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,12 @@
+import styles from "@/css/about.module.css";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div>
+      <p className={styles.text}>
+        Somos una agencia de diseño web con un enfoque minimalista que combina funcionalidad y
+        estética para brindar experiencias de usuario excepcionales.
+      </p>
+    </div>
+  );
 }
